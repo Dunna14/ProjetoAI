d2be462ea01e4dbffddaea38380da3255bd0d4f6
Miguel Campos <filipe422.11@gmail.com>
2022-06-20 13:42:25 +0100

    nao sei

--- resources/views/carrinho/index.blade.php
+++ resources/views/carrinho/index.blade.php
@@ -77,16 +77,18 @@
                 </table>
 
             </div>
-            
+
             @if (Auth::user())
                                         @if(is_null(Auth::user()->cliente))
-                                            <a href="{{ route('user.edit') }}">
+                                        @if (Auth::user()->tipo=='C')
+                                            <a href="{{ route('clientes.edit_cliente') }}">
                                                 <button type="button"
                                                     class="w-64 mt-6 text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
                                                     Tem de configurar o cliente</button>
                                             </a>
+                                        @endif
                                             @else
-                                            <a href="#" data-toggle="modal" data-target="#payLogal">            
+                                            <a href="#" data-toggle="modal" data-target="#payLogal">
                                             <button type="button"
                                                     class="w-64 mt-6 text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
                                                     Pagar</button>
